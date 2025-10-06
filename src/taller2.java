273682712c21869c36e9d1bb3aca414672b453b1
nini <ninibeth.perez@alumnos.ucn.cl>
2025-10-06 19:29:24 -0300

    primer commit

---
+++ src/taller2.java
@@ -0,0 +1,9 @@
+public class taller2 {
+    public static void main(String[] args) {
+        System.out.println("hola mundo :3");
+        System.out.println("ola :p");
+        
+        System.out.println("buena");
+        
+    }
+}
