50ac83d6efab0e87e24454e6f06200e80839a3e7
nini <ninibeth.perez@alumnos.ucn.cl>
2025-10-07 13:19:02 -0300

    segundo commit

--- src/taller2.java
+++ src/taller2.java
@@ -1,9 +1,11 @@
+package src;
 public class taller2 {
     public static void main(String[] args) {
         System.out.println("hola mundo :3");
         System.out.println("ola :p");
         
         System.out.println("buena");
-        
+        System.out.println("asjklasjlsj");
+        System.out.println("Cambio de prueba");
     }
 }
