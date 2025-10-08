b34de1e5a7e6408b9cbdd95ae80cb13289a7b997
nini <ninibeth.perez@alumnos.ucn.cl>
2025-10-07 23:16:10 -0300

    commit de prueba

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,34 @@ public class Main {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		leerUsuario();
+        leerPcs();
+
+
+		System.out.println("Bienvenido: ");
+
+        
 
 	}
 
-	private static void leerUsuario() throws FileNotFoundException {
+	private static void leerPcs() throws FileNotFoundException {
+        File file = new File("pcs.txt");
+		scan = new Scanner(file);
+		
+		while (scan.hasNextLine()) {
+            String linea = scan.nextLine();
+			String [] partes = linea.split(";");
+            String id = partes[0];
+            String ip = partes[1];
+            String so = partes[2];
+            Pc p = new Pc(id, ip, so);
+            pcs.add(p);
+            
+            voy a intentar subir los cambios q hemos hecho ahora, mira
+           
+        }
+    }
+
+    private static void leerUsuario() throws FileNotFoundException {
 		File file = new File("usuarios.txt");
 		scan = new Scanner(file);
 		
@@ -25,11 +49,8 @@ public class Main {
 			String contraseña = partes[1];
             String rol = partes[2];
             Usuario u = new Usuario(username, contraseña, rol);
-            
-			
-		}
-		
-		
+            usuarios.add(u);
+        }
 	}
 
 }
--- src/Puerto.java
+++ src/Puerto.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 
 public class Puerto {
+    private String pc;
 	private int numeroPuerto;
 	private String estado;
 	private ArrayList<Vulnerabilidad> vulnerabilidades;
 	
-	public Puerto(int numeroPuerto, String estado) {
+	public Puerto(String pc, int numeroPuerto, String estado) {
+        this.pc = pc;
 		this.numeroPuerto = numeroPuerto;
 		this.estado = estado;
 		this.vulnerabilidades = new ArrayList<>();
