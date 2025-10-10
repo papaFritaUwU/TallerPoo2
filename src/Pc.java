fec2ab7c001b8fe99344b1a853e98fdb0703d913
ninibeth pérez <ninibeth.perez@alumnos.ucn.cl>
2025-10-10 17:45:27 -0300

    descripciones de la clase pc

--- src/Pc.java
+++ src/Pc.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Pc {
+	// Representa un computador dentro de la red, con su id, dirección ip, sistema operativo y los puertos que posee.
 	private String id;
 	private String ip;
 	private String sO;
@@ -8,6 +9,8 @@ public class Pc {
 	
 	
 	public Pc(String id, String ip, String sO) {
+		//Es el constructor de la clase :3.
+		//Crea un nuevo PC con su id, ip y sistema operativo, iniciando una lista vacía de puertos.
 		this.id = id;
 		this.ip = ip;
 		this.sO = sO;
@@ -15,22 +18,27 @@ public class Pc {
 	}
 
 	public String getId() {
+		//Retorna el id del PC.
 		return id;
 	}
 	
 	public String getIp() {
+		//Retorna la dirección ip del PC.
 		return ip;
 	}
 
 	public String getsO() {
+		//Retorna el sistema operativo del PC.
 		return sO;
 	}
 
 	public ArrayList<Puerto> getPuertos() {
+		//Retorna la lista de puertos asociados al PC.
 		return puertos;
 	}
 
 	public void imprimirPC() {
+		//Imprime los datos del PC (ID, IP, sistema operativo) y todos sus puertos.
 		System.out.println("Id: " + id + " | IP: " + ip + " | Sistema Operativo: " + sO);
 		System.out.println("Puertos: ");
 		for (Puerto p : puertos) {
