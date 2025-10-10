aec665832bcd9e8f0c6ba02e00818480040f2339
ninibeth pérez <ninibeth.perez@alumnos.ucn.cl>
2025-10-10 15:15:55 -0300

    otras descripciones añadidas y correcciones ortográficas hechas

--- src/Main.java
+++ src/Main.java
@@ -17,11 +17,12 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
-	private static Scanner scan = new Scanner(System.in);
+	private static Scanner scan;
 	private static ArrayList<Pc> pcs = new ArrayList<>();
     private static ArrayList<Usuario> usuarios = new ArrayList<>();
     private static ArrayList<Puerto> puertos = new ArrayList<>();
 	private static String username;
+	
 	public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException {
 		leerUsuario();
         leerPcs();
@@ -29,50 +30,50 @@ public class Main {
         leerVulnerabilidades();
         
         scan = new Scanner(System.in);
-    
-        Boolean Estado = false;
-        Usuario Invitado = null;
+        Boolean estado = false;
+        Usuario invitado = null;
         do {
         	//Ciclo do-while de checkeo de credenciales correctas.
-        	Estado = true;
-        	
+        	estado = true;
             System.out.print("Usuario: ");
             username = scan.nextLine();
         	System.out.print("Contraseña: ");
             String contraseña = scan.nextLine();
-            Invitado = null;
-            
+            invitado = null;
             
             for(Usuario u: usuarios) {
-            	
             	if (u.getUsername().equals(username)) {
-            		Invitado = u;
-            		
-            		Estado = true;
+            		invitado = u;
+            		estado = true;
             		break;
+            		
             	}else {
-            		Estado = false;
+            		estado = false;
             	}
             }
-            if(Estado) {
-            	Boolean hash = EstadoHash(contraseña, Invitado);
+            
+            if(estado) {
+            	Boolean hash = EstadoHash(contraseña, invitado);
             	
             	if(hash) {
-            		Estado = true;
-            		System.out.println("Acceso otorgado");
+            		estado = true;
+            		System.out.println("Acceso otorgado :).");
             		
             	}else {
-            		Estado = false;
-            		System.out.println("Contraseña equivocada");
+            		estado = false;
+            		System.out.println("Acceso denegado.");
+            		System.out.println("Contraseña equivocada.");
+            		return;
             	}
-
             } else {
-            	System.out.println("Ingrese un usuario valido.");
+            	System.out.println("Acceso denegado.");
+            	System.out.println("Usuario equivocado.");
+            	return;
             }
 
-        }while(Estado == false);
+        }while(estado == false);
         
-        if(Invitado.getRol().equals("ADMIN")) {
+        if(invitado.getRol().equals("ADMIN")) {
         	menuAdmin();
         }else {
         	menuUsuario();
@@ -82,8 +83,7 @@ public class Main {
 	
 	private static Boolean EstadoHash(String contraseña, Usuario usuario) throws NoSuchAlgorithmException {
 		//Función tipo booleano que recibe la contraseña a revisar y el usuario que la escribio
-		//La función revisa la contraseña asociada al Usuario y la compara con la version Hasheada de la contraseña recibida.
-		
+				//La función revisa la contraseña asociada al Usuario y la compara con la version Hasheada de la contraseña recibida.
 		String PassWord = contraseña;
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] hashBytes = md.digest(PassWord.getBytes());
@@ -91,16 +91,14 @@ public class Main {
       
 		if(usuario.getContraseña().equals(textoHash)) {
 			return true;
-
 		}
-		
 		return false;
 	}
 
 	//---------------------------------- MENU ADMIN -------------------------------------------
 	
 	private static void menuAdmin() {
-		//En caso que el Usuario ingresado sea de rol Admin se habre este menu de opciones.
+		//En caso que el Usuario ingresado tenga el rol Admin se abre este menú de opciones.
 		scan = new Scanner(System.in);
         int opcion = -1;
         do {
@@ -118,9 +116,10 @@ public class Main {
                 	break;
                 	
                 case 2:
-                	System.out.println("¿Qué quiere hacer?");
+                	System.out.println("\n¿Qué quiere hacer?");
                 	System.out.println("1. Agregar un PC");
                 	System.out.println("2. Eliminar un PC");
+                	System.out.print(">> ");
                 	int eleccion = Integer.valueOf(scan.nextLine());
                 	
                 	if (eleccion == 1) {
@@ -145,13 +144,10 @@ public class Main {
         } while (opcion != 0);
     }
 	
-	
-	
 	private static void clasificarPCs() {
-		//Función de clasificaión de los PCs almacenados.
-		//Esta función explora los Puertos asignados a cada PC en la lista pcs y va sumando cada vulnerabilidad que poseen.
-		//Luego de la lectura entraga un veredicto de su nivel de riesgo y muestra por patalla cada puerto que posee una vulnerabilidad.
-		
+		//Función de clasificación de los PCs almacenados.
+		//Explora los puertos asignados a cada PC en la lista pcs y va sumando cada vulnerabilidad que poseen.
+		//Luego de la lectura entrega un veredicto de su nivel de riesgo y muestra por patalla cada puerto que posee una vulnerabilidad.
 		imprimirGuiones();
 		System.out.println("Nivel de riesgo de PCs");
 		int cantV = 0;
@@ -179,19 +175,72 @@ public class Main {
 	}
 	
 	private static void eliminarPC() {
-		// TODO Auto-generated method stub
-		
+		//Esta función permite al usuario eliminar un PC a elección.
+		//Explora en la lista de pcs para verificar si el PC ingresado es verídico.
+		//Si es que encuentra el PC, este se elimina de la lista de pcs y también se le eliminan los puertos asociados.
+		imprimirGuiones();
+		for (Pc pc : pcs) {
+			System.out.println("- " + pc.getId());
+		}
+		System.out.print("Ingrese el ID del PC a eliminar: ");
+		scan = new Scanner(System.in);
+        String idIngresado = scan.nextLine();
+        boolean encontrado = false;
+
+        for (int i = 0; i < pcs.size(); i++) {
+            if (pcs.get(i).getId().equals(idIngresado)) {
+                pcs.remove(i);
+                encontrado = true;
+                break;
+            }
+        }
+        for (int i = puertos.size() - 1; i >= 0; i--) {
+            if (puertos.get(i).getPc().equals(idIngresado)) {
+                puertos.remove(i);
+            }
+        }
+        if (encontrado) {
+        	System.out.println("PC y puertos asociados eliminados.");
+        } else {
+        	System.out.println("No se encontró un PC con ese ID.");
+        }
+        imprimirGuiones();
 	}
 
 	private static void agregarPC() {
-		// TODO Auto-generated method stub
-		
+		//Esta función permite al usuario agregar un PC. además de crear los puertos que desee.
+		imprimirGuiones();
+		scan = new Scanner(System.in);
+		System.out.print("ID del PC: ");
+        String id = scan.nextLine();
+        System.out.print("IP: ");
+        String ip = scan.nextLine();
+        System.out.print("Sistema operativo: ");
+        String so = scan.nextLine();
+
+        Pc pcNuevo = new Pc(id, ip, so);
+        pcs.add(pcNuevo);
+        
+        System.out.print("¿Cuántos puertos desea agregar al PC? ");
+        int cantPuertos = Integer.valueOf(scan.nextLine());
+        
+        for (int i = 0; i < cantPuertos; i++) {
+            System.out.print("Indique el número de puerto: ");
+            int numP = Integer.valueOf(scan.nextLine());
+            
+            System.out.print("Indique el estado (Abierto/Cerrado): ");
+            String estado = scan.nextLine();
+            Puerto p = new Puerto(id, numP, estado);
+            pcNuevo.getPuertos().add(p);
+            puertos.add(p); 
+        }
+        System.out.println("PC agregado correctamente.");
+        imprimirGuiones();
 	}
 
 	//---------------------------------- MENU USUARIO -------------------------------------------
 	private static void menuUsuario() {
-		//En caso que el Usuario ingresado sea de rol User se habre este menu de opciones.
-
+		//En caso que el usuario ingresado tenga el rol User se abre este menu de opciones.
 		scan = new Scanner(System.in);
         int opcion = -1;
         do {
@@ -239,19 +288,18 @@ public class Main {
 		imprimirGuiones();
 		for (Pc pc : pcs) {
 			pc.imprimirPC();
+			System.out.println();
 		} imprimirGuiones();
 	}
 
 	private static void escanearPC() {
 		//Función de escritura en el archivo reportes.txt.
 		//Esta función le solicita al usuario ingresar el PC a reportar, al igual que la hora del reporte.
-		//Escribe dentro del archivo txt mediante el comando (FileWriter("reportes.txt",true(True es para agregar texto al final del archivo)).
-		
+		//Escribe dentro del archivo txt mediante el comando (FileWriter("reportes.txt",true(True es para agregar texto al final del archivo))).
 		imprimirGuiones();
 		int i = 1;
 		for(Pc p: pcs) {
-			System.out.println(i++ + ") " + p);
-
+			System.out.println(i++ + ") " + p.getId());
 		}
 		scan = new Scanner(System.in);
 		int opcion = 0;
@@ -269,20 +317,18 @@ public class Main {
 		}
 		String aux;
 		
-		System.out.println("Si desea continuar con el Escaneo indique la fecha de hoy con el esquema (dia/mes/año), de lo contrario escriba SALIR: ");
+		System.out.println("Si desea continuar con el escaneo indique la fecha de hoy con el esquema (DD/MM/AAAA), de lo contrario escriba SALIR: ");
 		aux = scan.nextLine();
 			
-		if(aux.equals("SALIR")) {
-			System.out.println("Escaneo Cancelado");
-
+		if(aux.equalsIgnoreCase("salir")) {
+			System.out.println("Escaneo Cancelado.");
 		
 		}else {
 			try {
-				
 	            FileWriter escribir = new FileWriter("reportes.txt", true);
 	            
 	            escribir.write("------------------------------------------\n");
-	            escribir.write(PCdeseado.toString()+"\n");
+	            escribir.write(PCdeseado.getId()+"\n");
 	            escribir.write("Puertos correspondientes al PC: \n");
 	            
 	            for(Puerto p: PCdeseado.getPuertos()) {
@@ -304,56 +350,46 @@ public class Main {
 	            }
 	            escribir.write("Fecha del Escaneo: " + aux + "\n");
  
-	            
-	            
 	            escribir.close();
-	            System.out.println("Reporte creado con exito en el archivo reportes.txt");
+	            System.out.println("Reporte creado con exito en el archivo reportes.txt.");
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
 		}
+		imprimirGuiones();
     }
 
 	private static int nivelRiesgo(Pc pc) {
 		//Función auxiliar de escanearPC().
-		//Esta devuelve el nivel numerico de riesgo de un PC en especifico.
-		
+		//Esta devuelve el nivel numérico de riesgo de un PC en especifico.
 		int cantVulnerabilidad = 0;
-		
 		for(Puerto p: pc.getPuertos()) {
 			cantVulnerabilidad += p.getVulnerabilidades().size();
-			
 		}
-        	
-		
 		return cantVulnerabilidad;
 	}
 
 	private static void verTotalPuertosAbiertos() {
 		//Función de filtro de datos.
-		//Esta función utiliza dos for para leer par Puerto de cada PC, y si este puerto esta abierto, imprime sus datos asociados.
-		
+		//Esta función utiliza dos for para leer los puertos de cada PC, y si este puerto esta abierto, imprime sus datos asociados.
 		System.out.println("Total de puertos abiertos en todos los PCs de la red");
 		for (Pc pc : pcs) {
 			imprimirGuiones();
 			System.out.println("                                                       PC: " + pc.getId());
 			for (Puerto pu : pc.getPuertos()) {
-				if(pu.getEstado().equals("Abierto")) {
+				if (pu.getEstado().equals("Abierto")) {
 					System.out.println("Puerto: " + pu.getNumeroPuerto());
 					System.out.println("Vulnerabilidad | Descripción");
 					System.out.print("- ");
 					pu.imprimirVulnerabilidadesPuerto();
 				}
-				
 			}
 		}
 		imprimirGuiones();
 	}
 
 	private static void ordenarSegunIP() {
-		//Función de ordenamiento de los PC en pcs segun el IP asignado a cada uno.
-		
-		
+		//Función de ordenamiento de los PC en la lista pcs según la clase de IP asignado a cada uno.
 		ArrayList<Pc> pcsOrdenadas = new ArrayList<>(pcs);
 		ordenarPc(pcsOrdenadas);
 		imprimirGuiones();
@@ -366,8 +402,6 @@ public class Main {
 	private static String claseIp(String ip) {
 		//Función auxiliar de ordenarSegunIp().
 		//Esta función le asigna una "ClaseIP" a cada PC segun su Rango de IP.
-		//La función obtiene el primer valor de IP mediante la separación de ip.split("\\."); y utiliza este para la comparación de rango.
-		
 		String [] ipPartes = ip.split("\\.");
 		int ipParte1 = Integer.valueOf(ipPartes[0]);
 		if (0 <= ipParte1 & ipParte1 <= 127) {
@@ -381,7 +415,7 @@ public class Main {
 
 	private static void ordenarPc(ArrayList<Pc> pcsOrdenadas) {
 		//Función auxiliar de ordenarSegunIP().
-		//Esta función rellena la lista ArrayList pcsOrdenadas mediante dos ciclos For y la función de comparación de IP "compararIP"
+		//Esta función rellena la lista ArrayList pcsOrdenadas mediante dos ciclos for y la función de comparación de IP "compararIP"
 		for (int i = 0; i < pcs.size() - 1; i++) {
         	for (int j = i + 1; j < pcs.size(); j++) {
         		if (compararIp(pcsOrdenadas.get(i).getIp(), pcsOrdenadas.get(j).getIp())) {
@@ -396,8 +430,7 @@ public class Main {
 	private static boolean compararIp(String ip, String ip2) {
 		//Función auxiliar de ordenarPC().
 		//Esta función separa los numeros de la IP del PC a examinar mediante ".split("\\.")" para luego poder evaluarlos.
-		//La función revisa de izquiera a derecha los numeros asociados a cada split de la IP, y en base a la comparación de esos numeros retorna un booleano.
-		
+		//La función revisa de izquierda a derecha los numeros asociados a cada split de la IP, y en base a la comparación de esos numeros retorna un booleano.
 		String [] ipPartes = ip.split("\\.");
 		String [] ipPartes2 = ip2.split("\\.");
 		
@@ -422,7 +455,6 @@ public class Main {
 		//La función crea Vulnerabilidades de la clase Vulnerabilidad.
 		//Esta función separa cada linea del archivo mediante ".split" y ocupa los valores que se encuentran en el para satisfacer al constructor de la clase Vulnerabilidad.
 		//La función tambien las vulnerabilidades correspondientes a cada puerto mediante "for(Puerto p..)".
-		
 		File file = new File("vulnerabilidades.txt");
 		scan = new Scanner(file);
 		
@@ -447,7 +479,6 @@ public class Main {
 		//La función crea Puertos de la clase Puerto y los agrega a la ArrayList "puertos".
 		//La función tambien agrega a cada PC de la lista pcs sus puertos correspondientes.
 		//Esta función separa cada linea del archivo mediante ".split" y ocupa los valores que se encuentran en el para satisfacer al constructor de la clase Puerto.
-
         File file = new File("puertos.txt");
 		scan = new Scanner(file);
 		
@@ -473,8 +504,6 @@ public class Main {
     	//Lectura del archivo pcs.txt.
     	//La función crea PCs de la clase PC y los agrega a la ArrayList "pcs".
 		//Esta función separa cada linea del archivo mediante ".split" y ocupa los valores que se encuentran en el para satisfacer al constructor de la clase PC.
-
-    	
         File file = new File("pcs.txt");
 		scan = new Scanner(file);
 		
@@ -493,8 +522,6 @@ public class Main {
     	//Lectura del archivo usuarios.txt.
     	//La función crea Usuarios de la clase Usuario y los agrega a la ArrayList "usuarios".
 		//Esta función separa cada linea del archivo mediante ".split" y ocupa los valores que se encuentran en el para satisfacer al constructor de la clase Usuario.
-
-    	
 		File file = new File("usuarios.txt");
 		scan = new Scanner(file);
 		
@@ -510,7 +537,7 @@ public class Main {
 	}
     
     private static void imprimirGuiones() {
-    	//Función estetica de impresión de guiones de separación.
+    	//Función estetica de impresión de guiones de separación, para evitar escribirlos a cada rato. :3
     	System.out.println("-------------------------------------------------------------------------------------------------------------------");
     }
 }
