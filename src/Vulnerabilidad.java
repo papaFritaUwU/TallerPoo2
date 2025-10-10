cbcd301e93debf11872ae2f3a69b41f76640a650
ninibeth pérez <ninibeth.perez@alumnos.ucn.cl>
2025-10-10 17:47:04 -0300

    descripciones de la clase vulnerabilidad

--- src/Vulnerabilidad.java
+++ src/Vulnerabilidad.java
@@ -1,24 +1,30 @@
 
 public class Vulnerabilidad {
+	//Representa una vulnerabilidad de un puerto, indicando su nombre, descripción y puerto.
 	private int puerto;
 	private String nombreVulnerabilidad;
 	private String descripcion;
 	
 	public Vulnerabilidad(int puerto, String nombreV, String descripcion) {
+		//Es el constructor de la clase :3.
+		//Crea una nueva vulnerabilidad asociada a un puerto, con su nombre y descripción.
 		this.puerto = puerto;
 		this.nombreVulnerabilidad = nombreV;
 		this.descripcion = descripcion;
 	}
 
 	public String getNombreV() {
+		//Retorna el nombre de la vulnerabilidad.
 		return nombreVulnerabilidad;
 	}
 
 	public String getDescripcion() {
+		//Retorna la descripción de la vulnerabilidad.
 		return descripcion;
 	}
 	
 	public int getPuerto() {
+		//Retorna el número de puerto donde está la vulnerabilidad.
 		return puerto;
 	}
 }
