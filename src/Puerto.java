3f4a804fd8f8c1c7c280fb9e937d6c70b5062b35
ninibeth pérez <ninibeth.perez@alumnos.ucn.cl>
2025-10-10 17:45:57 -0300

    descripciones de la clase puerto

--- src/Puerto.java
+++ src/Puerto.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 
 public class Puerto {
+	//Representa un puerto de red asociado a un PC, junto con su estado y las vulnerabilidades detectadas en él.
     private String pc;
 	private int numeroPuerto;
 	private String estado;
 	private ArrayList<Vulnerabilidad> vulnerabilidades;
 	
 	public Puerto(String pc, int numeroPuerto, String estado) {
+		//Es el constructor de la clase :3.
+		//Crea un nuevo puerto asignado a un PC específico, indicando su número, estado y preparando su lista de vulnerabilidades.
         this.pc = pc;
 		this.numeroPuerto = numeroPuerto;
 		this.estado = estado;
@@ -14,24 +17,34 @@ public class Puerto {
 	}
 
 	public String getPc() {
+		//Retorna el ID del PC al que pertenece este puerto.
 		return pc;
 	}
 
 	public int getNumeroPuerto() {
+		//Retorna el número del puerto.
 		return numeroPuerto;
 	}
 
 	public String getEstado() {
+		//Retorna el estado del puerto (Abierto o Cerrado).
 		return estado;
 	}
 
 	public ArrayList<Vulnerabilidad> getVulnerabilidades() {
+		// Retorna la lista de vulnerabilidades asociadas a este puerto.
 		return vulnerabilidades;
 	}
 	
 	public void imprimirVulnerabilidadesPuerto() {
-		for (Vulnerabilidad v : vulnerabilidades) {
-			System.out.println(v.getNombreV() + " | " + v.getDescripcion());
+		// Imprime las vulnerabilidades asociadas al puerto con su nombre y descripción.
+		// Si no hay vulnerabilidades, muestra un mensaje indicándolo.
+		if (vulnerabilidades.size() == 0) {
+			System.out.println("No hay vulnerabilidades.");
+		} else {
+			for (Vulnerabilidad v : vulnerabilidades) {
+				System.out.println(v.getNombreV() + " | " + v.getDescripcion());
+			}
 		}
 	}
 
